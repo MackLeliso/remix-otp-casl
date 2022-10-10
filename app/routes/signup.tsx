aad80649e3e833af973e7114267a70c3eddca3e9
leliso <lelisoagegnehu491@gmail.com>
2022-10-10 16:22:15 +0300

    error display on ui

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -10,8 +10,7 @@ export const action: ActionFunction = async ({ request }) => {
     schema,
   });
   if (errors) {
-    console.log(errors);
-    return json(errors, { status: 400 });
+    return json({ errors }, { status: 400 });
   }
   const { first_name, last_name, phone } = formData;
   const field: Pick<User, "first_name" | "last_name" | "phone"> = {
@@ -23,9 +22,12 @@ export const action: ActionFunction = async ({ request }) => {
   console.log(field);
 
   const data = {};
+  return json({ data: "form" });
 };
 
 export default function signup() {
+  const actionData = useActionData();
+  console.log(actionData);
   return (
     <Box display="flex" justifyContent="center" p={5}>
       <Box minWidth={500} bgcolor="skyblue" borderRadius={2}>
@@ -41,20 +43,29 @@ export default function signup() {
               variant="filled"
               size="small"
             />
+            <Typography variant="subtitle2" color="error">
+              {actionData?.errors?.first_name}
+            </Typography>
             <TextField
               id="filled-hidden-label-small"
-              placeholder="First Name"
+              placeholder="Last Name"
               name="last_name"
               variant="filled"
               size="small"
             />
+            <Typography variant="subtitle2" color="error">
+              {actionData?.errors?.last_name}
+            </Typography>
             <TextField
               id="filled-hidden-label-small"
-              placeholder="First Name"
+              placeholder="Phone"
               name="phone"
               variant="filled"
               size="small"
             />
+            <Typography variant="subtitle2" color="error">
+              {actionData?.errors?.phone}
+            </Typography>
             <Button type="submit" variant="contained">
               Submit
             </Button>
