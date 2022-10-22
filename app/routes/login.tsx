904e74748be3bb0a5410946f1245b4b82708741d
leliso <lelisoagegnehu491@gmail.com>
2022-10-22 14:52:18 +0300

    login validation

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,10 @@
 import { Box, Button, Link, Stack, TextField, Typography } from "@mui/material";
 import { ActionFunction, json, redirect } from "@remix-run/node";
 import { Form, useTransition, useActionData } from "@remix-run/react";
+import { z } from "zod";
 import { verifyOtp, loginOtp } from "~/utils/otp.server";
 import { createUserSession } from "~/utils/session.server";
+import { validLogin } from "~/utils/validation.server";
 
 type ActonData = {
   phone: string;
@@ -11,23 +13,26 @@ type LoginData = {
   message: string;
   status: number;
 };
+
 export const action: ActionFunction = async ({ context, request }) => {
-  const { phone } = (await Object.fromEntries(
+  const fields = (await Object.fromEntries(
     await request.formData()
   )) as ActonData;
-  const { message, status } = (await loginOtp(phone)) as LoginData;
-  if (message === "otp-sent") {
-    return await createUserSession({ phone }, `/verify`);
-  }
-  return { message, phone };
+
+  // schema validation
+  const { success, data, field, fieldErrors } = await validLogin(fields);
+  if (!success) return { field, fieldErrors };
+  if (!success) return { field, fieldErrors };
+  const { message } = (await loginOtp(data.phone)) as LoginData;
+  if (message === "otp-sent")
+    return await createUserSession({ phone: data.phone }, `/verify`);
+  return { message };
 };
 
 export default function signIn() {
   const actionData = useActionData();
-  console.log(actionData);
   const { state } = useTransition();
   const busy = state === "submitting";
-  let m = 1;
 
   return (
     <Box display="flex" justifyContent="center" p={5}>
@@ -51,10 +56,10 @@ export default function signIn() {
               name="phone"
               variant="filled"
               size="small"
-              defaultValue={actionData?.phone}
+              defaultValue={actionData?.field?.phone}
             />
             <Typography variant="subtitle2" color="error">
-              {actionData?.errors?.phone}
+              {actionData?.fieldErrors?.phone}
             </Typography>
 
             <Button disabled={busy} type="submit" variant="contained">
