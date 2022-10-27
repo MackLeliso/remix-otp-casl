168d55facdb09b284bf1b7c4319b2031807f8621
leliso <lelisoagegnehu491@gmail.com>
2022-10-27 13:39:26 +0300

    create product

--- app/utils/product.server.ts
+++ app/utils/product.server.ts
@@ -1,12 +1,79 @@
 import { db } from "./db.server";
 
-export const createProduct = async (id: string) => {};
+type ProductData = {
+  name: string;
+  column: string;
+  operator: string;
+  value: string;
+  search: string;
+  field: string;
+  skip: number;
+  sort: string;
+  limit: number;
+};
+export const getProducts = async (data: ProductData) => {
+  try {
+    const { name, column, operator, value, search, field, skip, sort, limit } =
+      data;
+    const [productList, totalCount] = await db.$transaction([
+      db.product.findMany({
+        where: {
+          delete: false,
+          category: {
+            name: {
+              contains: name,
+            },
+          },
+          [column]: { [operator]: value || search },
+        },
+        orderBy: { [field]: sort },
+        skip: skip,
+        take: limit,
+      }),
+      db.product.count({
+        where: { delete: false },
+      }),
+    ]);
+
+    return { productList, totalCount };
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+export const createProduct = async (productData: any) => {
+  try {
+    const product = await db.product.create({
+      data: productData,
+    });
+    return product;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
 export const updateProduct = async (id: string) => {};
 
-export const deleteProduct = async () => {
+export const deleteProduct = async (id: string) => {
   try {
-    const product = await db.product.findMany();
-    console.log("product deleted", product);
+    return await db.$transaction(async () => {
+      const getProduct = await db.product.findFirst({
+        where: {
+          id,
+          delete: false,
+        },
+      });
+      if (!getProduct) return { status: 404, message: "Product not found" };
+      await db.product.update({
+        where: { id },
+        data: { delete: true },
+      });
+      return {
+        status: 200,
+        message: "Successfully product is deleted",
+      };
+    });
   } catch (error) {
     console.log("error", error);
   }
---
+++ prisma/migrations/20221026201748_add_price/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "picture" STRING;
+ALTER TABLE "Product" ADD COLUMN     "price" STRING;
